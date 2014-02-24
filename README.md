1c49ae68f91c5e51ae26984e5680dedd9fadc23b
Eimantas <eim.kasp@gmail.com>
2014-02-24 20:53:51 +0200

    Added all site files

---
+++ README.md
@@ -0,0 +1,7 @@
+About FoursquareInsta
+===============
+
+You can look for photos of actual location on Instagram by using Foursquare venue ID
+
+If you want to extend your applications with deeper social integration and to gather data from both Foursquare and Instagram at the same time, you can look up for Instagram ID's for your foursquare venues
+If you are interested in source code and want to use it on your own website, feel free to grab it from GitHub
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,31 @@ function instagramAJAXCall(place) {
             $("#instagram_id span").text(id);
             $("#instagram_name span").text(data.data[0].name);
             $("#instagram_info").fadeIn();
+            instagramGetPhotos(id);
             
     }
-    
-
 });
+
+function instagramGetPhotos(index) {
+    
+        $.ajax({
+            type: "GET",
+            dataType: "jsonp",
+            cache: true,
+            url: "https://api.instagram.com/v1/locations/" + index + "/media/recent?MIN_TIMESTAMP=1391216400&client_id=1720573181104199bb04ff29990457c2",
+            success: function (data) {
+                data.data.forEach(function(item){
+                    $("#instagram_photos").append("<div class='photo'><img src='" + item.images.low_resolution.url + "'/></div>");
+
+                });
+                
+                console.log(data);
+            }, error: function () {
+            }
+        });
+    }
 }
+
 $(document).ready(function(){
     $('.form-inline').submit(function(e){
         instagramAJAXCall($("#foursquareID").val());
